22be912cc5fb0da26a5a6cecef306f9ce86acd6c
tamilvanan <tamilvanansambasivam@gmail.com>
2024-12-19 20:30:02 +0530

    simple array

---
+++ 19-12-2024/simple dsa/array.js
@@ -0,0 +1,15 @@
+let arr = [1, 2, 3, 4, 5];
+
+// Accessing elements
+console.log(arr[0]); // Output: 1
+
+// Adding elements
+arr.push(6); // Adds 6 at the end
+arr.unshift(0); // Adds 0 at the beginning
+
+// Removing elements
+arr.pop(); // Removes 6
+arr.shift(); // Removes 0
+
+// Searching elements
+console.log(arr.indexOf(3)); // Output: 2 (index of 3)
