586dcbc9f72318bf461e515add2fd1fb89bc75d5
tamilvanan <tamilvanansambasivam@gmail.com>
2024-12-23 14:47:03 +0530

    update solution

--- 23-12-2024/217. Contains Duplicate/solution.js
+++ 23-12-2024/217. Contains Duplicate/solution.js
@@ -4,8 +4,8 @@
  */
 var containsDuplicate = function (nums) {
   nums.sort();
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] == nums[i - 1]) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == nums[i + 1]) {
       return true;
     }
   }
